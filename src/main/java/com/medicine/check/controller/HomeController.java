0f707684be8b606f4cb67828a7d6bd4f2fdf71fe
eee269 <skfkd3123@gmail.com>
2021-01-20 00:23:52 +0900

    url html명으로 바꿈(테스트용)

--- src/main/java/com/medicine/check/controller/HomeController.java
+++ src/main/java/com/medicine/check/controller/HomeController.java
@@ -9,4 +9,32 @@ public class HomeController {
     public String Home() {
         return "index";
     }
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+    @GetMapping("/blog-details")
+    public String blog_details() {
+        return "blog-details";
+    }
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+    @GetMapping("/listing")
+    public String listing() {
+        return "listing";
+    }
+    @GetMapping("/listing-details")
+    public String listing_details() {
+        return "listing-details";
+    }
 }
