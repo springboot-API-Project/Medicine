08adf84559f89fb51fe9fffe0fc25fa03e0c33b0
eee269 <skfkd3123@gmail.com>
2021-02-18 14:10:47 +0900

    Feat: View 수정, thymeleaf 적용 (index.html, about.html 는 내용 바꿔야 함 -> 회의때 다시 논의, blog.html, blog-details.html 는 기초작업 완료)

--- src/main/java/com/medicine/check/controller/HomeController.java
+++ src/main/java/com/medicine/check/controller/HomeController.java
@@ -2,38 +2,39 @@ package com.medicine.check.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String Home() {
-        return "/index";
+        return "index";
     }
-    @GetMapping("/main")
+    @GetMapping("main")
     public String main() {
         return "main";
     }
-    @GetMapping("/about")
+    @GetMapping("about")
     public String about() {
         return "about";
     }
-    @GetMapping("/blog")
+    @GetMapping("blog")
     public String blog() {
         return "blog";
     }
-    @GetMapping("/blog-details")
+    @GetMapping("blog-details")
     public String blog_details() {
         return "blog-details";
     }
-    @GetMapping("/contact")
+    @GetMapping("contact")
     public String contact() {
         return "contact";
     }
-    @GetMapping("/listing")
+    @GetMapping("listing")
     public String listing() {
         return "listing";
     }
-    @GetMapping("/listing-details")
+    @GetMapping("listing-details")
     public String listing_details() {
         return "listing-details";
     }
